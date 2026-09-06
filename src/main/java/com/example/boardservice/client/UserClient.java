c3c795333c066dcbc1f2d7782fd102b93c81004f
KyeongMinBae <reaaaaaaal@gmail.com>
2026-09-06 18:42:11 +0900

    36. [실습] 포인트 차감 -> 게시글 작성 -> 활동 점수 적립 순으로 로직이 처리되도록 만들기

--- src/main/java/com/example/boardservice/client/UserClient.java
+++ src/main/java/com/example/boardservice/client/UserClient.java
@@ -1,8 +1,10 @@
 package com.example.boardservice.client;
 
+import com.example.boardservice.dto.AddActivityScoreRequestDto;
 import com.example.boardservice.dto.UserResponseDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
@@ -50,5 +52,17 @@ public class UserClient {
             // 로깅 코드
             return Collections.emptyList();
         }
+    }
+
+    public void addActivityScore(Long userId, int score) {
+        AddActivityScoreRequestDto addActivityScoreRequestDto =
+                new AddActivityScoreRequestDto(userId, score);
+
+        this.restClient.post()
+                .uri("/users/activity-score/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(addActivityScoreRequestDto)
+                .retrieve()
+                .toBodilessEntity();
     }
 }
